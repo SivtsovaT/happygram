1240aa47e4f90ea6f95dffe47718d8171f274906
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2023-01-19 17:35:07 +0200

    THG - 001 Create new Firebase project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+	apiKey: "AIzaSyCeUC2aJIE0yMl0Pp3upvcVFESMN6TkyI0",
+	authDomain: "happygran-b33fc.firebaseapp.com",
+	projectId: "happygran-b33fc",
+	storageBucket: "happygran-b33fc.appspot.com",
+	messagingSenderId: "174567403326",
+	appId: "1:174567403326:web:4765f9e01c4eb6017ec66f",
+	measurementId: "G-9CFS5BWFLM"
+};
+const app = initializeApp(firebaseConfig);
