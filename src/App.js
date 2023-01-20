5ea6311bd07fbb350a23d6c521ab7a92fd27630d
user <sivtsovatanya1985@gmail.com>
2023-01-20 10:50:33 +0200

    THG - 006 Sign in by email
    - added Signin  page;
    - updated Splash page & Signup page;

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './index.scss';
 import {Route, Routes} from "react-router-dom";
 import SplashPage from "./splash-page/SplashPage";
 import SignUpPage from "./signup-page/SignUpPage";
+import SignInPage from "./signin-page/SignInPage";
 const App = () => {
   return (
       <>
           <Routes>
               <Route path="/" element={<SplashPage/>}/>
               <Route path="signup" element={<SignUpPage/>}/>
+              <Route path="signin" element={<SignInPage/>}/>
           </Routes>
       </>
   )
