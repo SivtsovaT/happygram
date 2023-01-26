a712a3ce6a2359c2301fcacd8b6681bd91334f37
user <sivtsovatanya1985@gmail.com>
2023-01-26 14:36:44 +0200

    THG - 013 Complete UI for sending messages (text-only). Display incoming/outgoing messages side by side. Prettify date times.
    - added a MessagesPage.js;

--- src/messages-page/MessagesPage.js
+++ src/messages-page/MessagesPage.js
@@ -1,33 +1,58 @@
 import React, {useEffect, useState} from "react";
 import "./MessagesPage.scss";
-import {addDoc, collection, getDocs, query, serverTimestamp, where, orderBy} from "firebase/firestore";
+import {addDoc, collection, getDocs, query, serverTimestamp, orderBy} from "firebase/firestore";
 import {db} from "../firebase";
 import {getAuth} from "firebase/auth";
+import back from "../images/back.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPaperPlane} from "@fortawesome/free-solid-svg-icons/faPaperPlane";
+import moment from "moment";
 
 const MessagesPage = ({contactDisplayName, contactId, showHome}) => {
-	//const [users, setUsers] = useState([]);
 	const [currentAuth, setCurrentAuth] = useState(null);
-	//const [userData, setUserData] = useState([]);
 	const [inputMessage, setInputMessage] = useState('');
 	const [httpPending, setHttpPending] = useState(false);
-	const [message, setMessage] = useState([]);
-	const [sentMessages, setSentMessages] = useState([]);
-	//const [messages, setMessages] = useState([]);
+	const [conId, setConId] = useState([]);
+	const [usId, setUsId] = useState([]);
+	const [messages, setMessages] = useState([]);
+
+	const sentStyles = {
+		display: "flex",
+		justifyContent: "flex-end",
+
+	}
+	const receivedStyles = {
+		display: "flex",
+		justifyContent: "flex-start",
+	}
+
+	const sentMessagesBackground = {
+		background: "#FAEBD7"
+	}
+
+	const receivedMessagesBackground = {
+		background: "#E0FFFF"
+	}
+    const [date, setDate] = useState('');
 
 	useEffect(() => {
-		const getSentMessages = async () => {
-			const q = query(collection(db, "messages"), where("contactId", "==", contactId), orderBy("created"));
+		const auth = getAuth();
+		const userId = auth.currentUser.uid;
+		const getSeMessages = async () => {
+			const q = query(collection(db, "messages"), orderBy("created"));
 			const querySnapshot = await getDocs(q);
 			querySnapshot.forEach((doc) => {
-				setSentMessages(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+				setMessages(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 			});
+			await setConId(contactId);
+			await setUsId(userId);
 		};
-		getSentMessages();
+		getSeMessages();
 	}, [currentAuth]);
+
 
 	const sendMessage = async (e) => {
 		e.preventDefault();
-		const items = message.push(inputMessage);
 		 const auth = getAuth();
 		const userId = auth.currentUser.uid;
 		if (httpPending) {
@@ -37,17 +62,17 @@ const MessagesPage = ({contactDisplayName, contactId, showHome}) => {
 		try {
 			if (inputMessage.length > 0) {
 				await addDoc(collection(db, 'messages'), {
-					items,
 					text: inputMessage,
 					userId: userId,
 					contactId: contactId,
 					created: serverTimestamp()
 				}, {merge: true});
-				const q = query(collection(db, "messages"), where("contactId", "==", contactId), orderBy("created"));
+				const q = query(collection(db, "messages"), orderBy("created"));
 				const querySnapshot = await getDocs(q);
 				querySnapshot.forEach((doc) => {
-					setSentMessages(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+					setMessages(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
 				});
+
 				setHttpPending(false);
 				setInputMessage('');
 			} else {
@@ -61,28 +86,51 @@ const MessagesPage = ({contactDisplayName, contactId, showHome}) => {
 
 	return (
 		<div className="content">
-			<button onClick={showHome}>BACK</button>
-			<div>{contactDisplayName}</div>
-			<button onClick={sendMessage}>send</button>
-			<input type="text"
-				   value={inputMessage}
-				   placeholder="write your message"
-				   onChange={(event) => setInputMessage(event.target.value)}
-			/>
-			{
-				sentMessages.map(message => {
-					return (
-						<div key={message.id}>
-							{
-								httpPending && <div>Wait...</div>
-							}
-							{
-								!httpPending && <div>{message.text}</div>
-							}
-						</div>
-					)
-				})
-			}
+			<div onClick={showHome} className="link-panel">
+				<img src={back} alt="back"/>
+			</div>
+			<div className="project-header">{contactDisplayName}</div>
+			<div className="message-list">
+				{
+					messages.map(mes => {
+						return (
+							<div key={mes.id}>
+								{
+									mes.userId == usId && mes.contactId == conId ?
+										<div style={sentStyles}>
+											<div style={sentMessagesBackground} className="message-wrapper">
+												<div className="message-text">{mes.text}</div>
+												<div
+													className="message-time">{moment(mes.created.toDate()).calendar()}</div>
+											</div>
+										</div> :
+										mes.userId == conId && mes.contactId == usId ?
+											<div style={receivedStyles}>
+												<div style={receivedMessagesBackground} className="message-wrapper">
+													<div className="message-text">{mes.text}</div>
+													<div
+														className="message-time">{moment(mes.created.toDate()).calendar()}</div>
+												</div>
+											</div>
+											: null
+								}
+							</div>
+						)
+					})
+				}
+			</div>
+			<div className="send-input-wrapper">
+				<input type="text"
+					   className="input-log"
+					   style={{height:"36px", width:"270px", borderRadius:"5px"}}
+					   value={inputMessage}
+					   placeholder="write your message"
+					   onChange={(event) => setInputMessage(event.target.value)}
+				/>
+				<button className="send-btn" onClick={sendMessage}>
+					<FontAwesomeIcon style={{width:"20px", height:"20px"}} icon={faPaperPlane}/>
+				</button>
+			</div>
 		</div>
 	)
 }
