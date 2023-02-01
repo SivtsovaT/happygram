b9ada016ad77376cc1d763a4993c2c994884e045
user <sivtsovatanya1985@gmail.com>
2023-02-01 17:24:26 +0200

    THG - 013 added esLint

--- src/my-contacts-page/MyContactsPage.js
+++ src/my-contacts-page/MyContactsPage.js
@@ -1,153 +1,162 @@
-import React, {useEffect, useState} from "react";
-import "./MyContactsPage.scss";
-import {getAuth} from "firebase/auth";
-import {db} from "../firebase";
-import {doc, collection, getDocs, getDoc} from "firebase/firestore";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
-import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons/faRightFromBracket";
-import avatar from "../images/search-page/avatar.jpg";
-import MessagesPage from "../messages-page/MessagesPage";
-import {Link} from "react-router-dom";
-import back from "../images/back.png";
+import React, { useEffect, useState } from 'react';
+import './MyContactsPage.scss';
+import { getAuth } from 'firebase/auth';
+import {
+  doc, collection, getDocs, getDoc,
+} from 'firebase/firestore';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons/faRightFromBracket';
+import { Link } from 'react-router-dom';
+import avatar from '../images/search-page/avatar.jpg';
+import MessagesPage from '../messages-page/MessagesPage';
+import { db } from '../firebase';
+import back from '../images/back.png';
 
-const MyContactsPage = () => {
-	const [searchValue, setSearchValue] = useState('');
-	const [currentAuth, setCurrentAuth] = useState(null);
-	const [userData, setUserData] = useState([]);
-	const [contacts, setContacts] = useState([]);
-	const [filteredContacts, setFilteredContacts] = useState([]);
-	const [contactDisplayName, setContactDisplayName] = useState('');
-	const [invisibleHome, setInvisibleHome] = useState(false);
-	const [contactId, setContactId] = useState('');
+function MyContactsPage() {
+  const [searchValue, setSearchValue] = useState('');
+  const [currentAuth, setCurrentAuth] = useState(null);
+  const [userData, setUserData] = useState([]);
+  const [contacts, setContacts] = useState([]);
+  const [filteredContacts, setFilteredContacts] = useState([]);
+  const [contactDisplayName, setContactDisplayName] = useState('');
+  const [invisibleHome, setInvisibleHome] = useState(false);
+  const [contactId, setContactId] = useState('');
 
-	const stylesHome = {
-		display: invisibleHome ? "none" : "flex"
-	}
+  const stylesHome = {
+    display: invisibleHome ? 'none' : 'flex',
+  };
 
-	const getAuthUser = async () => {
-		try {
-			const auth = await getAuth();
-			const userId = auth?.currentUser?.uid || null
-			setCurrentAuth(userId);
-			if (!userId) {
-				setTimeout(() => {
-					getAuthUser();
-				}, 2000);
-			}
-		} catch (e) {
-			console.log(e)
-		}
-	};
+  const getAuthUser = async () => {
+    try {
+      const auth = await getAuth();
+      const userId = auth?.currentUser?.uid || null;
+      setCurrentAuth(userId);
+      if (!userId) {
+        setTimeout(() => {
+          getAuthUser();
+        }, 2000);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-	useEffect(() => {
-		if (!currentAuth) {
-			return;
-		}
-		const getUser = async () => {
-			const docRef = doc(db, "users", currentAuth);
-			const docSnap = await getDoc(docRef);
-			const data = docSnap.data();
-			setUserData(data);
-		}
-		getUser();
-	}, [currentAuth]);
+  useEffect(() => {
+    if (!currentAuth) {
+      return;
+    }
+    const getUser = async () => {
+      const docRef = doc(db, 'users', currentAuth);
+      const docSnap = await getDoc(docRef);
+      const data = docSnap.data();
+      setUserData(data);
+    };
+    getUser();
+  }, [currentAuth]);
 
-	useEffect(() => {
-		const auth = async () => {
-			await getAuthUser();
-		};
-		auth();
-	}, []);
+  useEffect(() => {
+    const auth = async () => {
+      await getAuthUser();
+    };
+    auth();
+  }, []);
 
-	useEffect(() => {
-		if (!currentAuth) {
-			return;
-		}
-		const getContacts = async () => {
-			const data = await getDocs(collection(db, `users/${currentAuth}/contacts`));
-			setContacts(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-		};
-		getContacts();
-	}, [currentAuth]);
+  useEffect(() => {
+    if (!currentAuth) {
+      return;
+    }
+    const getContacts = async () => {
+      const data = await getDocs(collection(db, `users/${currentAuth}/contacts`));
+      // eslint-disable-next-line no-shadow
+      setContacts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getContacts();
+  }, [currentAuth]);
 
-	useEffect(() => {
-		setFilteredContacts(
-			contacts.filter(
-				(contact) =>
-					contact.displayName.toLowerCase().includes(searchValue.toLowerCase())
-			)
-		);
-	}, [searchValue, contacts]);
+  useEffect(() => {
+    setFilteredContacts(
+      contacts.filter(
+        (contact) => contact.displayName.toLowerCase().includes(searchValue.toLowerCase()),
+      ),
+    );
+  }, [searchValue, contacts]);
 
-	const showHome = () => {
-		setInvisibleHome(prevState => !prevState)
-	}
+  const showHome = () => {
+    setInvisibleHome((prevState) => !prevState);
+  };
 
-	const showContact = async (id) => {
-		const contactId = id;
-		const contactRef = doc(db, `users/${currentAuth}/contacts/${contactId}`);
-		const docSnap = await getDoc(contactRef);
-		await setContactDisplayName(docSnap.data().displayName);
-		await setContactId(contactId);
-		setInvisibleHome(true);
-	}
+  const showContact = async (id) => {
+    // eslint-disable-next-line no-shadow
+    const contactId = id;
+    const contactRef = doc(db, `users/${currentAuth}/contacts/${contactId}`);
+    const docSnap = await getDoc(contactRef);
+    await setContactDisplayName(docSnap.data().displayName);
+    await setContactId(contactId);
+    setInvisibleHome(true);
+  };
 
-	const logout = () => {
-	  window.location.replace("/signin");
-	}
+  const logout = () => {
+    window.location.replace('/signin');
+  };
 
-	return (
-		<>
-			{
-				invisibleHome && <MessagesPage contactDisplayName={contactDisplayName}
-											   contactId={contactId}
-											   showHome={showHome}
-				/>
-			}
-			<div style={stylesHome} className="content">
-				<div className="users-header">
-					<div className="password-wrapper">
-						<input
-							style={{height:"36px", width:"100px"}}
-							type="text"
-							className="input-log"
-							value={searchValue}
-							onChange={(event) => setSearchValue(event.target.value)}
-						/>
-						<FontAwesomeIcon icon={faMagnifyingGlass} style={{marginLeft:"-30px"}}/>
-					</div>
-					<div className="project-main">
-						Welcome, {userData.displayName}!
-					</div>
-					<FontAwesomeIcon onClick={logout} style={{width:"30px", height:"30px"}} icon={faRightFromBracket}/>
-				</div>
-				<Link to="/search" className="link-panel">
-					<img src={back} alt="back"/>
-				</Link>
+  return (
+    <>
+      {
+            invisibleHome && (
+            <MessagesPage
+              contactDisplayName={contactDisplayName}
+              contactId={contactId}
+              showHome={showHome}
+            />
+            )
+        }
+      <div style={stylesHome} className="content">
+        <div className="users-header">
+          <div className="password-wrapper">
+            <input
+              style={{ height: '36px', width: '100px' }}
+              type="text"
+              className="input-log"
+              value={searchValue}
+              onChange={(event) => setSearchValue(event.target.value)}
+            />
+            <FontAwesomeIcon icon={faMagnifyingGlass} style={{ marginLeft: '-30px' }} />
+          </div>
+          <div className="project-main">
+            Welcome,
+            {' '}
+            {userData.displayName}
+            !
+          </div>
+          <FontAwesomeIcon onClick={logout} style={{ width: '30px', height: '30px' }} icon={faRightFromBracket} />
+        </div>
+        <Link to="/search" className="link-panel">
+          <img src={back} alt="back" />
+        </Link>
 
-				<div className="users-list">
-					{
-						filteredContacts.map(contact => {
-							return (
-								<div key={contact.id}
-									 className="user-wrapper"
-								>
-									<img onClick={() => showContact(contact.id)}
-										 className="user-avatar" src={avatar} alt="avatar"/>
-									<div className="user-info">
-										<div className="project-main">{contact.displayName}</div>
-										<div className="user-email">{contact.email}</div>
-									</div>
-								</div>
-							)
-						})
-					}
-				</div>
-				<button className="btn btn-295">CONTINUE</button>
-			</div>
-		</>
-	)
+        <div className="users-list">
+          {
+              filteredContacts.map((contact) => (
+                <div
+                  key={contact.id}
+                  className="user-wrapper"
+                >
+                  <button style={{ backgroundColor: 'rgba(28,28,28,0)', border: 'none' }} type="button" onClick={() => showContact(contact.id)}>
+                    <img className="user-avatar" src={avatar} alt="avatar" />
+                  </button>
+                  <div className="user-info">
+                    <div className="project-main">{contact.displayName}</div>
+                    <div className="user-email">{contact.email}</div>
+                  </div>
+                </div>
+              ))
+            }
+        </div>
+        <button type="button" className="btn btn-295">CONTINUE</button>
+      </div>
+    </>
+  );
 }
 
 export default MyContactsPage;
--- src/signIn-by-phone-page/SignInByPhonePage.js
+++ src/signIn-by-phone-page/SignInByPhonePage.js
@@ -1,146 +1,141 @@
-import React, {useEffect, useState} from "react";
-import "./SignInByPhonePage.scss";
-import {Link} from "react-router-dom";
-import back from "../images/back.png";
-import app from "../firebase"
-import {getAuth, RecaptchaVerifier, signInWithPhoneNumber} from "firebase/auth";
+import React, { useEffect, useState } from 'react';
+import './SignInByPhonePage.scss';
+import { Link } from 'react-router-dom';
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import back from '../images/back.png';
+import app from '../firebase';
 
+function SignInByPhonePage() {
+  const countryCode = '+38';
+  const [phoneNumber, setPhoneNumber] = useState(countryCode);
+  const [OTP, setOTP] = useState('');
+  const [otpVisible, setOtpVisible] = useState(false);
+  const [phoneVisible, setPhoneVisible] = useState(true);
+  const [counter, setCounter] = useState(60);
 
-const SignInByPhonePage = () => {
-	const countryCode = "+38";
-	const [phoneNumber, setPhoneNumber] = useState(countryCode);
-	const [OTP, setOTP] = useState('');
-	const [otpVisible, setOtpVisible] = useState(false);
-	const [phoneVisible, setPhoneVisible] = useState(true);
-	const [counter, setCounter] = useState(60);
+  const otpStyles = {
+    display: otpVisible ? 'block' : 'none',
+  };
 
-	const otpStyles = {
-		display: otpVisible ? "block" : 'none'
-	}
+  const phoneStyles = {
+    display: phoneVisible ? 'block' : 'none',
+  };
 
-	const phoneStyles = {
-		display: phoneVisible ? 'block' : "none"
-	}
+  const showPhone = () => {
+    setOtpVisible(false);
+    setPhoneVisible(true);
+  };
 
-	const showPhone = () => {
-		setOtpVisible(false);
-		setPhoneVisible(true);
-	}
+  const handleChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
 
-	const handleChange = (event) => {
-		setPhoneNumber(event.target.value);
-	}
+  const auth = getAuth(app);
 
-	const auth = getAuth(app);
+  const generateRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+      size: 'invisible',
+      callback: () => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+      },
+    }, auth);
+  };
+  const requestOtp = (event) => {
+    event.preventDefault();
+    if (phoneNumber.length === 13) {
+      generateRecaptcha();
+      const appVerifier = window.recaptchaVerifier;
+      signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+          window.confirmationResult = confirmationResult;
+          setOtpVisible(true);
+          setPhoneVisible(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  const verifyOtp = (event) => {
+    const otp = event.target.value;
+    setOTP(otp);
+    if (otp.length === 6) {
+      const { confirmationResult } = window;
+      confirmationResult.confirm(otp).then((result) => {
+        const { user } = result;
+        console.log(user);
+        setTimeout(() => {
+          window.location.replace('home');
+        }, 1000);
+      }).catch(((error) => {
+        console.log(error.message);
+      }));
+    }
+  };
+  useEffect(() => {
+    if (counter > 1) {
+      setTimeout(() => setCounter(counter - 1), 1000);
+    } else {
+      window.location.replace('signin');
+    }
+  }, [counter]);
 
+  return (
+    <div className="content">
+      <Link to="/signin" className="link-panel">
+        <img src={back} alt="back" />
+      </Link>
 
-	const generateRecaptcha = () => {
-		window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
-			'size': 'invisible',
-			'callback': (response) => {
-				// reCAPTCHA solved, allow signInWithPhoneNumber.
-			}
-		}, auth);
-	}
-	const requestOtp = (event) => {
-		event.preventDefault();
-		if (phoneNumber.length === 13) {
-			generateRecaptcha();
-			let appVerifier = window.recaptchaVerifier;
-			signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-				.then(confirmationResult => {
-					window.confirmationResult = confirmationResult;
-					setOtpVisible(true);
-					setPhoneVisible(false);
-				})
-				.catch((error) => {
-					console.log(error);
-				})
-		}
-	}
-	const verifyOtp = (event) => {
-		let otp = event.target.value;
-		setOTP(otp);
-		if (otp.length === 6) {
-			let confirmationResult = window.confirmationResult;
-			confirmationResult.confirm(otp).then((result) => {
-				const user = result.user;
-				console.log(user);
-				setTimeout(() => {
-					window.location.replace("home");
-				}, 1000);
+      <div style={phoneStyles} className="signup-wrapper">
 
-			}).catch((error => {
-				console.log(error.message)
-			}))
-		}
-	}
-	useEffect(() => {
-		if (counter > 1) {
-			setTimeout(() => setCounter(counter - 1), 1000);
+        <div id="recaptcha-container" />
+        <div className="project-main">
+          <div className="enter-phone">Enter your phone number here</div>
+        </div>
+        <div style={{ position: 'relative' }} className="input-phone-wrapper">
+          <input
+            className="input-log height-67"
+            type="text"
+            placeholder="Enter phone number"
+            value={phoneNumber}
+            onChange={handleChange}
+          />
+          <button type="button" style={{ top: '570px', left: '6px' }} onClick={requestOtp} className="btn btn-295">Send phone number</button>
+        </div>
+      </div>
+      <div style={otpStyles} className="signup-wrapper">
+        <button type="button" className="link-panel" onClick={showPhone}>
+          <img src={back} alt="back" />
+        </button>
 
-		} else {
-			window.location.replace("signin")
-		}
-	}, [counter]);
+        <div className="otp-header">
+          <div className="otp-title">
+            <div className="otp-value">00</div>
+            <div className="otp-value">:</div>
+            {
+              counter < 10 ? <div className="otp-value">0</div> : null
+            }
+            <div className="otp-value">{counter}</div>
+          </div>
+          <div className="descr">
+            Type the verification code
+            we’ve sent you
+          </div>
+        </div>
+        <div className="otp-input-wrapper">
+          <input
+            className="input-log height-67"
+            type="text"
+            placeholder="Otp"
+            value={OTP}
+            onChange={verifyOtp}
+          />
+        </div>
+        <button type="button" onClick={showPhone} className="send-again">Send again</button>
+      </div>
 
-	return (
-		<div className="content">
-			<Link to="/signin" className="link-panel">
-				<img src={back} alt="back"/>
-			</Link>
-
-			<div style={phoneStyles} className="signup-wrapper">
-
-				<div id="recaptcha-container"></div>
-					<div className="project-main">
-						<div className="enter-phone">Enter your phone number here</div>
-					</div>
-				<div style={{position:"relative"}} className="input-phone-wrapper">
-					<input className="input-log height-67"
-						   type="text"
-						   placeholder="Enter phone number"
-						   value={phoneNumber}
-						   onChange={handleChange}
-					/>
-					{/*<Link to="/menu">*/}
-					<button style={{top: "570px", left:"6px"}} onClick={requestOtp} className="btn btn-295">Send phone number</button>
-
-					{/*</Link>*/}
-				</div>
-			</div>
-			<div style={otpStyles} className="signup-wrapper">
-				{/*<div  className="link-panel">*/}
-					<div className="link-panel" onClick={showPhone}>
-						<img src={back} alt="back"/>
-					</div>
-				{/*</div>*/}
-
-				<div className="otp-header">
-					<div className="otp-title">
-						<div className="otp-value">00</div>
-						<div className="otp-value">:</div>
-						{
-							counter < 10 ? <div className="otp-value">0</div> : null
-						}
-						<div className="otp-value">{counter}</div>
-					</div>
-					<div className="descr">Type the verification code
-						we’ve sent you</div>
-				</div>
-				<div className="otp-input-wrapper">
-					<input className="input-log height-67"
-						   type="text"
-						   placeholder="Otp"
-						   value={OTP}
-						   onChange={verifyOtp}
-					/>
-				</div>
-				<div onClick={showPhone} className="send-again">Send again</div>
-			</div>
-
-		</div>
-	)
+    </div>
+  );
 }
 
 export default SignInByPhonePage;
