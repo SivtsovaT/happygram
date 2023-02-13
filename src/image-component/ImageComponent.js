03fdb0332ea2e254d904d8fb124f7e288b29408c
SivtsovaT <66840978+SivtsovaT@users.noreply.github.com>
2023-02-13 09:28:44 +0200

    THG-014-2 - Allow sending messages with attachments (#17)
    - added sending files;
    - changed app's background;
    - changed some styles;

--- src/image-component/ImageComponent.js
+++ src/image-component/ImageComponent.js
@@ -6,9 +6,10 @@ import { faClose } from '@fortawesome/free-solid-svg-icons';
 function ImageComponent({ showMessages, messageImage }) {
   return (
     <div className="content-messages">
-      <button className="image-btn" type="button" onClick={showMessages}>
+      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+      <div className="image-btn" onClick={showMessages} onKeyUp={showMessages}>
         <FontAwesomeIcon icon={faClose} style={{ width: '20px', height: '20px' }} />
-      </button>
+      </div>
       <img className="message-image" src={messageImage} alt="message_image" />
     </div>
   );
